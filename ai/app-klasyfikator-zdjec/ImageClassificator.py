381f1fe559f7e401b780f05b1eb4324985f9102c
EC2 Default User <ec2-user@ip-172-31-5-177.eu-west-1.compute.internal>
2021-09-07 17:30:47 +0000

    ok

---
+++ ai/app-klasyfikator-zdjec/ImageClassificator.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+import time
+
+sagemakerRuntime = boto3.client('sagemaker-runtime')
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+
+    time.sleep(2)
+
+    categories = event["categories"].split(" ");
+    endpoint = event["endpointName"]
+    bucket = "REPLACEME_NAZWA_TWOJEGO_BUCKET_S3"
+    image = "validate.png";
+
+    s3.meta.client.download_file(bucket, image, "/tmp/"+image)
+
+    img = open("/tmp/"+image, 'rb').read()
+
+    response = sagemakerRuntime.invoke_endpoint(
+        EndpointName=endpoint,
+        ContentType='application/x-image',
+        Body=bytearray(img)
+    )
+
+    result = response['Body'].read()
+    result = json.loads(result)
+
+    items = [];
+
+    it = -1
+    for score in result:
+        it = it + 1;
+        if (score > categoryScore):
+            categoryScore = score
+            category = categories[it];
+
+        item = {"category": categories[it], "score":score}
+        items.append(item);
+
+    print(category);
+    print(categoryScore);
+
+
+    return {
+        'winner': category,
+        'score':categoryScore,
+        'items' : items
+    }
